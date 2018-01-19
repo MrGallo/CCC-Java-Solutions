4154b29f96b5fa2db0bdf867736b6ca8e179fd54
MrGallo <daniel.gallo@ycdsbk12.ca>
2018-01-19 13:23:35 -0500

    Add test and solution for 2017 Junior 1

--- 2017 Junior 1/src/Main.java
+++ 2017 Junior 1/src/Main.java
@@ -1,5 +1,22 @@
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println(1);
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        int x = scan.nextInt();
+        int y = scan.nextInt();
+
+        System.out.println(QuadrantSelect(x, y));
+    }
+
+    public static int QuadrantSelect(int x, int y) {
+        if (x > 0 && y > 0)
+            return 1;
+        else if (x < 0 & y > 0)
+            return 2;
+        else if (x < 0 && y < 0)
+            return 3;
+        else
+            return 4;
     }
 }
---
+++ 2017 Junior 1/src/QuadrantSelectTest.java
@@ -0,0 +1,25 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class QuadrantSelectTest {
+    @Test
+    public void quadrant1_for_1_1() {
+        assertEquals(1, Main.QuadrantSelect(1, 1));
+    }
+
+    @Test
+    public void quadrant2_for_neg1_1() {
+        assertEquals(2, Main.QuadrantSelect(-1, 1));
+    }
+
+    @Test
+    public void quadrant3_for_neg1_neg1() {
+        assertEquals(3, Main.QuadrantSelect(-1, -1));
+    }
+
+    @Test
+    public void quadrant4_for_1_neg1() {
+        assertEquals(4, Main.QuadrantSelect(1, -1));
+    }
+}
