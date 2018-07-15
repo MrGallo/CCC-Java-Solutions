8c379922ddb1fa9fa661f543891db9b95cafb36f
Mr. Gallo <daniel.gallo@ycdsbk12.ca>
2018-07-14 22:23:21 -0400

    save progress

--- 2018 Junior 5/Main.java
+++ 2018 Junior 5/Main.java
@@ -1,9 +1,40 @@
+import java.util.ArrayList;
+
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 public class Main {
+    public static HashMap<Integer, ArrayList<Integer>> bookMap;
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         String result = scanner.nextLine();
         System.out.println(result);
+    }
+
+
+    public static int shortestPathOfPage(int page) {
+        HashMap<Integer, ArrayList<Integer>> bookMapClone = new HashMap<>(bookMap);
+        return shortestPathOfPageHelper(page, bookMapClone);
+    }
+
+    private static int shortestPathOfPageHelper(int page, HashMap<Integer, ArrayList<Integer>> bookMap) {
+        HashMap<Integer, ArrayList<Integer>> bookMapClone = new HashMap<>(bookMap);
+        ArrayList<Integer> pageOptions = bookMapClone.get(page);
+        ArrayList<Integer> pageOptionsClone = new ArrayList<>(pageOptions);
+
+        if (pageOptions.contains(0))
+            return 1;
+
+        int minPath = Integer.MAX_VALUE;
+        for (Integer option : pageOptionsClone) {
+            pageOptions.remove(option);
+            int path = 1 + shortestPathOfPageHelper(option, bookMapClone);
+            if (path < minPath)
+                minPath = path;
+        }
+
+        return minPath;
     }
 }
--- 2018 Junior 5/TestChooseOwnPath.java
+++ 2018 Junior 5/TestChooseOwnPath.java
@@ -1,10 +1,61 @@
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.fail;
 
 public class TestChooseOwnPath {
+
     @Test
-    public void test() {
-        fail("Failure!");
+    public void testShortestPathOfOnePageIs1() {
+        Main.bookMap = new HashMap<>();
+        Main.bookMap.put(1, new ArrayList<Integer>() {{ add(0); }});
+
+        assertEquals(1, Main.shortestPathOfPage(1));
+    }
+
+    @Test
+    public void testShortestPathOfTwoPagesProperlyLinkedIs2() {
+        Main.bookMap = new HashMap<>();
+        Main.bookMap.put(1, new ArrayList<Integer>() {{ add(2); }});
+        Main.bookMap.put(2, new ArrayList<Integer>() {{ add(0); }});
+
+        assertEquals(2, Main.shortestPathOfPage(1));
+    }
+
+    @Test
+    public void testShortestPathOfThreePagesNoShortcutsIs3() {
+        Main.bookMap = new HashMap<>();
+        Main.bookMap.put(1, new ArrayList<Integer>() {{ add(2); }});
+        Main.bookMap.put(2, new ArrayList<Integer>() {{ add(3); }});
+        Main.bookMap.put(3, new ArrayList<Integer>() {{ add(0); }});
+
+        assertEquals(3, Main.shortestPathOfPage(1));
+    }
+
+    @Test
+    public void testShortestPathOfThreePagesWithShortcutsIs2() {
+        Main.bookMap = new HashMap<>();
+        Main.bookMap.put(1, new ArrayList<Integer>() {{ add(2); add(3); }});
+        Main.bookMap.put(2, new ArrayList<Integer>() {{ add(3); }});
+        Main.bookMap.put(3, new ArrayList<Integer>() {{ add(0); }});
+
+        assertEquals(2, Main.shortestPathOfPage(1));
+    }
+
+    @Test
+    public void testShortestPathShouldNotLoopBetweenPages1And4() {
+        Main.bookMap = new HashMap<>();
+        // Program should not loop
+        Main.bookMap.put(1, new ArrayList<Integer>() {{ add(2); add(4); }});
+        Main.bookMap.put(2, new ArrayList<Integer>() {{ add(3); }});
+        Main.bookMap.put(3, new ArrayList<Integer>() {{ add(0); }});
+        Main.bookMap.put(4, new ArrayList<Integer>() {{ add(1); }});
+
+        assertEquals(3, Main.shortestPathOfPage(1));
     }
 }
