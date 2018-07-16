a90c2c8f02e0e52773f7989251cbc521eb610bbd
robuntu <robuntu@stro.ycdsb.ca>
2018-07-16 18:23:40 -0400

    Refactor

--- 2018 Junior 5/TestChooseOwnPath.java
+++ 2018 Junior 5/TestChooseOwnPath.java
@@ -1,9 +1,6 @@
 import org.junit.jupiter.api.Test;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.fail;
@@ -13,7 +10,7 @@ public class TestChooseOwnPath {
     @Test
     public void testShortestPathOfOnePageIs1() {
         Main.bookMap = new HashMap<>();
-        Main.bookMap.put(1, new ArrayList<Integer>() {{ add(0); }});
+        Main.bookMap.put(1, new ArrayList<>(Arrays.asList(0)));
 
         assertEquals(1, Main.shortestPathOfPage(1));
     }
@@ -21,8 +18,8 @@ public class TestChooseOwnPath {
     @Test
     public void testShortestPathOfTwoPagesProperlyLinkedIs2() {
         Main.bookMap = new HashMap<>();
-        Main.bookMap.put(1, new ArrayList<Integer>() {{ add(2); }});
-        Main.bookMap.put(2, new ArrayList<Integer>() {{ add(0); }});
+        Main.bookMap.put(1, new ArrayList<>(Arrays.asList(2)));
+        Main.bookMap.put(2, new ArrayList<>(Arrays.asList(0)));
 
         assertEquals(2, Main.shortestPathOfPage(1));
     }
@@ -30,9 +27,9 @@ public class TestChooseOwnPath {
     @Test
     public void testShortestPathOfThreePagesNoShortcutsIs3() {
         Main.bookMap = new HashMap<>();
-        Main.bookMap.put(1, new ArrayList<Integer>() {{ add(2); }});
-        Main.bookMap.put(2, new ArrayList<Integer>() {{ add(3); }});
-        Main.bookMap.put(3, new ArrayList<Integer>() {{ add(0); }});
+        Main.bookMap.put(1, new ArrayList<>(Arrays.asList(2)));
+        Main.bookMap.put(2, new ArrayList<>(Arrays.asList(3)));
+        Main.bookMap.put(3, new ArrayList<>(Arrays.asList(0)));
 
         assertEquals(3, Main.shortestPathOfPage(1));
     }
@@ -40,9 +37,9 @@ public class TestChooseOwnPath {
     @Test
     public void testShortestPathOfThreePagesWithShortcutsIs2() {
         Main.bookMap = new HashMap<>();
-        Main.bookMap.put(1, new ArrayList<Integer>() {{ add(2); add(3); }});
-        Main.bookMap.put(2, new ArrayList<Integer>() {{ add(3); }});
-        Main.bookMap.put(3, new ArrayList<Integer>() {{ add(0); }});
+        Main.bookMap.put(1, new ArrayList<>(Arrays.asList(2, 3)));
+        Main.bookMap.put(2, new ArrayList<>(Arrays.asList(3)));
+        Main.bookMap.put(3, new ArrayList<>(Arrays.asList(0)));
 
         assertEquals(2, Main.shortestPathOfPage(1));
     }
@@ -51,10 +48,10 @@ public class TestChooseOwnPath {
     public void testShortestPathShouldNotLoopBetweenPages1And4() {
         Main.bookMap = new HashMap<>();
         // Program should not loop
-        Main.bookMap.put(1, new ArrayList<Integer>() {{ add(2); add(4); }});
-        Main.bookMap.put(2, new ArrayList<Integer>() {{ add(3); }});
-        Main.bookMap.put(3, new ArrayList<Integer>() {{ add(0); }});
-        Main.bookMap.put(4, new ArrayList<Integer>() {{ add(1); }});
+        Main.bookMap.put(1, new ArrayList<>(Arrays.asList(2, 4)));
+        Main.bookMap.put(2, new ArrayList<>(Arrays.asList(3)));
+        Main.bookMap.put(3, new ArrayList<>(Arrays.asList(0)));
+        Main.bookMap.put(4, new ArrayList<>(Arrays.asList(1)));
 
         assertEquals(3, Main.shortestPathOfPage(1));
     }
